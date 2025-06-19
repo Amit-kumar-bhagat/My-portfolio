ef261dc2e0d3b86ff9eb92e1cc31c39c4f871c52
Amit Kumar Bhagat <amitkumarrawstar801@gmail.com>
2025-06-20 01:24:07 +0530

    Fix: Add Fresh PrototypeIcon.jsx with correct casing

---
+++ src/components/PrototypeIcon.jsx
@@ -0,0 +1,11 @@
+import prototypeicon from "../assets/prototypeIcon.svg";
+
+const PrototypeIcon = () => {
+  return (
+    <div id="prototypeIcon" class="absolute w-34 h-34 top-55 left-18">
+      <img src={prototypeicon} alt="prototypeIcon" class="w-28 h-28" />
+    </div>
+  );
+};
+
+export default PrototypeIcon;
