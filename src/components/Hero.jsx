8f058f525cbe588eb381167c0eb38eda69c42566
Amit Kumar Bhagat <amitkumarrawstar801@gmail.com>
2025-06-20 00:25:15 +0530

    Initial commit: Add personal portfolio made with React and Tailwind

---
+++ src/components/Hero.jsx
@@ -0,0 +1,82 @@
+import react from "../assets/react.svg";
+import banner from "../assets/banner.svg";
+import figma from "../assets/figma.svg";
+import whatsapp from "../assets/whatsapp.svg";
+import linkedin from "../assets/linkedin.svg";
+import gmail from "../assets/gmail.svg";
+
+
+const Hero = () => {
+  return (
+    <div
+      id="main-container"
+      class="flex w-auto h-104  rounded-2xl "
+    >
+      <div
+        id="Hero-container "
+        class="p-12 pr-0 w-180  h-94 border  border-none  flex "
+      >
+        <div
+          id="Intro-container"
+          class="w-full h-auto border-none text-left p-0 text-white"
+        >
+          <h1 class="text-3xl font-bold">Hi, I am </h1>
+          <h1 class="text-4xl font-bold ">Amit Kumar Bhagat </h1>
+          <p class="mt-4 font-light text-shadow-white">
+            Blending creativity with technical expertise, I craft custom UX
+            designs that deliver real results. Let's turn your vision into a
+            seamless, user-friendly experience.
+          </p>
+          <div class="border-none inline-block w-auto  mt-4 gap-4">
+            <button class=" bg-white w-[114.35px] h-[37.6px] border-none text-black   px-4 py-1.5">
+              {" "}
+              Hire me
+            </button>
+            <button class=" ml-2 border border-green-500 px-4 py-1.5 justify-center hover:bg-green-500 cursor-pointer">
+              {" "}
+              Know more
+            </button>
+          </div>
+
+          <div class="react-and-para border-none ml-0  mt-4 flex p-0 space-x-3">
+            <div>
+              <img
+                id="react-logo"
+                src={react}
+                alt="react logo"
+                class="h-10 w-10"
+              />{" "}
+            </div>
+            <div class="flex items-center justify-center ">
+              <p class="text-sm">
+                {" "}
+                Visual Designer and developer Specialized in UI/UX
+              </p>{" "}
+            </div>
+          </div>
+
+          <div id="social-icons" class=" w-64 h-auto mt-5 flex space-x-4 items-center">
+
+            <div id="figma" class=""> <a href="https://www.figma.com/@amitkumarbhagat"><img src={figma} alt="figma-icon" class="w-7 h-7" /></a></div>
+            <div id="whatsapp" class=""><a href="https://wa.me/919876543210"><img src={whatsapp} alt="figma-icon" class="w-7 h-7" /></a></div>
+            <div id="linkedin" class=""><a href="https://linkedin.com/in/amitkumarbhagat881"><img src={linkedin} alt="figma-icon" class="w-7 h-7" /></a></div>
+            <div id="email" class=""><a href="mailto:amitkumarrawstar801@gmail.com"><img src={gmail} alt="figma-icon" class="w-8 h-8" /></a></div>
+
+          </div>
+
+
+        </div>
+      </div>
+
+      <div id="banner-image" class=" w-[920px]  ml-34 h-auto border-amber-300 flex text-right">
+        <img
+          src={banner}
+          alt="Banner-image"
+          class="w-full h-full object-cover transform -translate-y-9 translate-x-16"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,47 @@
+import logo from "../assets/logo.png";
+
+const Navigation = () => {
+  return (
+
+    <div id="header" class="border rounded-full border-green-500/80">
+    <header class="text-gray-600 body-font">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center text-white">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <img src={logo} alt="My logo" class="w-12 h-12" />
+          <span class="ml-3 text-xl text-white">Amit Kumar Bhagat</span>
+        </a>
+        <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a href="#" class="mr-5 hover:text-green-600 cursor-pointer">
+            Home
+          </a>
+          <a href="#" class="mr-5 hover:text-green-600 cursor-pointer">
+            Portfolio
+          </a>
+          <a href="#" class="mr-5 hover:text-green-600 cursor-pointer">
+            Services
+          </a>
+          <a href="#" class="mr-5 hover:text-green-600 cursor-pointer">
+            About
+          </a>
+        </nav>
+        <button class=" cursor-pointer text-black font-semibold inline-flex items-center bg-green-600 border-0 py-2 px-5 focus:outline-none hover:bg-green-800 transition-all delay-100 rounded-full text-base mt-4 md:mt-0">
+          Contact us
+          {/* <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg> */}
+        </button>
+      </div>
+    </header>
+    </div>
+  )
+};
+
+export default Navigation;
